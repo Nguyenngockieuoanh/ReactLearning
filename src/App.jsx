cf2540f8db3b6167c45d7047c5a6b132b566968d
Nguyenngockieuoanh <nguyenngockieuoanh0902@gmail.com>
2026-03-08 14:38:15 +0700

    IfElse

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,23 @@
 import React from "react";
 import Products from "./components/Products";
+import Category from "./components/Category";
+import NotCategory from "./components/NotCategory";
 
 function App() {
-  const listProducts = [
-    {
-      url: "https://down-vn.img.susercontent.com/file/vn-11134258-81ztc-mlg2h0zvt9fn9b",
-      title: "Product 1",
-      price: 1000,
-    },
-    {
-      url: "https://down-vn.img.susercontent.com/file/vn-11134258-81ztc-mlg2h0zvt9fn9b",
-      title: "Product 2",
-      price: 1200,
-    },
-    {
-      url: "https://down-vn.img.susercontent.com/file/vn-11134258-81ztc-mlg2h0zvt9fn9b",
-      title: "Product 3",
-      price: 1300,
-    },
-    {
-      url: "https://down-vn.img.susercontent.com/file/vn-11134258-81ztc-mlg2h0zvt9fn9b",
-      title: "Product 4",
-      price: 1400,
-    },
-  ];
-
-  return (
-    <div style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
-      {listProducts.map((product, index) => {
-        return (
-          <Products
-            title={product.title}
-            price={product.price}
-            url={product.url}
-            key={index}
-          />
-        );
-      })}
-    </div>
-  );
+  const isRenderCategory = false;
+  if (isRenderCategory) {
+    return (
+      <div>
+        <Category />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <NotCategory />
+      </div>
+    );
+  }
 }
 
 export default App;
