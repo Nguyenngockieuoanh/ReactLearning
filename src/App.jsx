0a9a7db546db085a649bc88a0922262a861fe440
Nguyenngockieuoanh <nguyenngockieuoanh0902@gmail.com>
2026-03-08 17:50:21 +0700

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Products from "./components/Products";
 import Category from "./components/Category";
-import NotCategory from "./components/NotCategory";
+
 import Message from "./components/Message";
 
 function App() {
-  return <Category />;
+  const [count, setcount] = useState(() => {
+    const number = 1 + 1 + 1;
+    return number;
+  });
+  console.log("sss");
+  const handleClick = () => {
+    setcount((prev) => prev + 1);
+  };
+
+  return (
+    <>
+      <div>count : {count}</div>
+      <button onClick={handleClick}>Click here</button>
+    </>
+  );
 }
 
 export default App;
